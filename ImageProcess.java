69a0e455295097fe0ecacf3c4d7d17c9885f873d
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2018-10-04 17:14:25 +0530

    New Branch

--- ImageProcess.java
+++ ImageProcess.java
@@ -1,5 +1,7 @@
 public class ImageProcess{
     public static void main(String[] args){
         System.out.println("Image Prosessing method");
+
+        System.out.println("New Branch");
     }
 }
